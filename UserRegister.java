9816381911f4d132ce38f7a84020757935482312
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-22 19:27:14 +0530

    ADD:Enter valid first name

--- UserRegister.java
+++ UserRegister.java
@@ -1,7 +1,31 @@
-class UserRegister
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class UserRegister 
 {
-public static void main(String args[])
-{
-System.out.println("Welcome to UserRegister");
+	static String fName;
+	
+	static String firstName()
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Fisrt name:");
+		fName=scan.nextLine();
+		return fName;
+		
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to User Registration Problem");
+		
+		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegister.firstName());
+		
+		while(firstName == false)
+		{
+			System.out.println("Error ! Invalid input from user please enter your First-Name in valid format('A'nkita)");
+			firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegister.firstName());
+		}
+		System.out.println("Fisrt Name :" +fName);
+	}
 }
-}
+
